5d33cf06d645231b4af444a45888d424e1e78a55
fenrees <dashakonik@mail.ru>
2019-02-05 22:33:50 +0300

    ch

--- homework04/api.py
+++ homework04/api.py
@@ -1,62 +1,55 @@
 import requests
 import time
-
-from config import VK_CONFIG as vk
-from typing import Union, List, Optional
-from api_models import User, Message
+import config
 
 
-
-def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3)-> Optional[requests.models.Response]:
+def get(url: str, params={}, timeout=1, max_retries=5, backoff_factor=0.3):
     """ Выполнить GET-запрос
-
-    :param url: адрес, на который необходимо выполнить запрос
+    :param url: адрес, на который необходимо выполнить запросas_edgelist
     :param params: параметры запроса
     :param timeout: максимальное время ожидания ответа от сервера
     :param max_retries: максимальное число повторных запросов
     :param backoff_factor: коэффициент экспоненциального нарастания задержки
     """
-    for t in range(max_retries):
+    for i in range(max_retries):
         try:
-            response = requests.get(url, params=params, timeout=(timeout, 3))
-            return response
+            req = requests.get(url, params=params, timeout=timeout)
+            return req
         except requests.exceptions.RequestException:
-            if t == max_retries - 1:
+            if i == max_retries - 1:
                 raise
-            delay = backoff_factor * 2 ** t
-            # time.sleep прерывает работу скрипта на определнное кол-во времени (в данное время - откладывается)
-            time.sleep(delay)
+            value = backoff_factor * (2 ** i)
+            time.sleep(value)
 
-def get_friends(user_id, fields)-> Union[List[User], List[int]]:
+
+def get_friends(user_id: int, fields="") -> dict:
     """ Вернуть данных о друзьях пользователя
-
     :param user_id: идентификатор пользователя, список друзей которого нужно получить
     :param fields: список полей, которые нужно получить для каждого пользователя
     """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
-    # параметры запроса к серваку
+
     query_params = {
-        'access_token': vk['access_token'],
+        'domain': config.VK_CONFIG['domain'],
+        'access_token': config.VK_CONFIG['access_token'],
         'user_id': user_id,
         'fields': fields,
-        'version': vk['version']
+        'v': config.VK_CONFIG['version']
     }
-    # запрос делается, в {} вставляется домен из конфига (import config)
-    query = "{domain}/friends.get?".format(domain=vk['domain'])
-    # получение ответа
+
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v={v}".format(
+        **query_params)
     response = get(query, query_params)
-    # переделывание в джейсон
-    json_doc = response.json()
-    fail = json_doc.get('error')
-    if fail != 0:
-        return json_doc['response']
+    json_file = response.json()
+    fail = json_file.get('error')
+    if fail:
+        raise Exception(json_file['error']['error_msg'])
+    return json_file['response']['items']
 
-
-def messages_get_history(user_id: int, offset=0, count=200)-> list:
+def messages_get_history(user_id: int, offset=0, count=200) -> list:
     """ Получить историю переписки с указанным пользователем
-
     :param user_id: идентификатор пользователя, с которым нужно получить историю переписки
     :param offset: смещение в истории переписки
     :param count: число сообщений, которое нужно получить
@@ -66,33 +59,29 @@ def messages_get_history(user_id: int, offset=0, count=200)-> list:
     assert isinstance(offset, int), "offset must be positive integer"
     assert offset >= 0, "user_id must be positive integer"
     assert count >= 0, "user_id must be positive integer"
-
-    # очередные параметры к серваку
     query_params = {
-        'domain': vk['domain'],
-        'access_token': vk['access_token'],
+        'domain': config.VK_CONFIG['domain'],
+        'access_token': config.VK_CONFIG['access_token'],
         'user_id': user_id,
         'offset': offset,
-        'count': count,
-        'version': vk['version']
+        'messages_count': min(count, 200),
+        'v': config.VK_CONFIG['version']
     }
+
     messages = []
-    i = 0
-    while i < count:
-        if (i / 200) % 3 == 0 and i:
-            time.sleep(1)
-        if count - i <= 200:
-            query_params['count'] = count - i
-        url = "{domain}/messages.getHistory?offset={offset}&count={count}&user_id={user_id}&" \
-              "access_token={access_token}&v={version}".format(**query_params)
-        # ответ =
-        response = requests.get(url)
-        # переделывание ответов в джейсон
-        json_doc = response.json()
-        fail = json_doc.get('error')
-        if fail:
-            raise Exception(json_doc['error']['error_msg'])
-        messages.extend(json_doc['response']['items'])
-        i += 200
-        query_params['offset'] += i
+    while count > 0:
+        query = "{domain}/messages.getHistory?" \
+            "access_token={access_token}&user_id={user_id}&offset={offset}&count={messages_count}&v={v}"\
+            .format(**query_params)
+        response = get(query, query_params)
+        if response:
+            json_file = response.json()
+            if json_file.get('error') is not None:
+                print(json_file['error']['error_msg'])
+            else:
+                messages.extend(json_file['response']["items"])
+        count -= min(count, 200)
+        query_params['offset'] += 200
+        query_params['messages_count'] = min(count, 200)
+        time.sleep(0.4)
     return messages
